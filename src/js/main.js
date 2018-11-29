8346c39ba7571d92e65b40e140f959017841da68
vikki <ivikki.dev@gmail.com>
2018-11-29 20:54:42 +0200

    markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
-//import '../css/style.scss';
+$(document).ready(function(){
+
+    var slider = $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
+        fade: true
+    });
+
+    $('.btn-play').click(function(){
+        if ($(this).hasClass('pause')) {
+            $(this)
+                .removeClass('pause')
+                .addClass('play')
+                .text('Play')
+            ;
+
+            slider.slick('slickPause');
+        } else {
+            $(this)
+                .removeClass('play')
+                .addClass('pause')
+                .text('Pause')
+            ;
+
+            slider.slick('slickPlay');
+        }
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -4,74 +4,93 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.resolve(__dirname, "src"),
-  entry: {
-    'js/main.js': './js/main.js',
-    'style.css': './css/style.scss'
-  },
-  output: {
-    filename: '[name]',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
+    context: path.resolve(__dirname, "src"),
+    entry: {
+        'js/main.js': './js/main.js',
+        'style.css': './css/style.scss'
+    },
+    output: {
+        filename: '[name]',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: "inline-source-map",
+    module: {
+        rules: [
             {
-              loader: 'css-loader',
-              options: {
-                //sourceMap: true
-              }
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
             },
             {
-              loader: 'sass-loader',
-              options: {
-                //sourceMap: true
-              }
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                        },
+                        {
+                            loader: 'postcss-loader'
+                        }
+                    ]
+                })
             },
             {
-              loader: 'postcss-loader'
-            }
-          ]
+                test: /\.(png|jp(e*)g|svg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'img/[name].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
+                test: /\.mp4$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'video/'
+                    }
+                }]
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        attrs: ['img:src', 'video:poster', 'source:src']
+                    }
+                }
+            },
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
         })
-      },
-      {
-        test: /\.(png|jp(e*)g|svg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8000, // Convert images < 8kb to base64 strings
-            name: 'img/[name].[ext]'
-          }
-        }]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
-      },
-    ]
-  },
+    ],
 
-  plugins: [
-    new ExtractTextPlugin('style.css'),
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-    })
-  ],
-
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-  }
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+    }
 };
